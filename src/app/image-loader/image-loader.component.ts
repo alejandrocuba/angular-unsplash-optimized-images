2b925ad1c51551a6590759f3ffd5df77f8e53001
ZorphDark <zorphdark@gmail.com>
2023-04-02 12:07:29 -0400

    use NgOptimizedImage directive

--- src/app/image-loader/image-loader.component.ts
+++ src/app/image-loader/image-loader.component.ts
@@ -1,11 +1,27 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, ImageLoaderConfig, IMAGE_LOADER, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'image-loader',
   standalone: true,
-  imports: [CommonModule],
-  template: ``
-  })
-export class ImageLoaderComponent {
-}
+  imports: [CommonModule, NgOptimizedImage],
+  providers: [
+    {
+      provide: IMAGE_LOADER,
+      useValue: (config: ImageLoaderConfig) => {
+        return `https://images.unsplash.com/${config.src}?auto=format&w=${config.width}&q=80`;
+      },
+    },
+  ],
+  styleUrls: ['./image-loader.component.scss'],
+  template: `
+    <img
+      ngSrc="photo-1675946581335-7ce800416a95"
+      width="800"
+      height="450"
+      alt="image"
+      priority
+    >
+  `
+})
+export class ImageLoaderComponent { }
